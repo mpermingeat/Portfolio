fd350cc4d838062a1d2e29a036e9ee4f0e0957b9
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-20 18:31:03 -0300

    Create Landing.jsx

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className=" flex bg-slate-900 w-vw h-vh text-white place-content-center items-center min-h-screen">
+      Landing
+    </div>
+  );
+}
+
+export default Landing;
