5ff8308e5c377a4e8d7089f3813042b0570c022e
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-21 11:29:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Landing from "./pages/Landing/Landing";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App bg-slate-900 min-h-screen">
+    <div className="App bg-primaryDark min-h-screen w-full">
+      <Navbar></Navbar>
       <Landing></Landing>
     </div>
   );
