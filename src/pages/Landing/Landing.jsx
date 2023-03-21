00bf22f72ca47541649879e99c56f20c4f8a4a22
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-21 11:29:39 -0300

    Update Landing.jsx

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Landing() {
   return (
-    <div className=" flex bg-slate-900 w-vw h-vh text-white place-content-center items-center min-h-screen">
-      Landing
+    <div className=" flex bg-primaryDark  text-primaryLight justify-center items-center  ">
+      <div className="w-3/4">
+        <h1 className=" text-primaryLight  font-bold text-8xl my-20">
+          I'm Maximiliano Permingeat - full stack developer.
+        </h1>
+        <div></div>
+      </div>
     </div>
   );
 }
