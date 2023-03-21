0f1f26ba0f8f53916341484cc3eeb5bc2ae1eef3
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-21 11:29:24 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDark: "#01161E",
+        secondaryDark: "#124559",
+        middleColor: "#598392",
+        primaryLight: "#EFF6E0",
+        secondaryLight: "#AEC3B0",
+      },
+    },
   },
   plugins: [],
 };
