6bb29625d996add99c10228f41a6be50cd944d9c
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-27 11:33:49 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,34 +1,29 @@
-function Card() {
+function Card(props) {
   return (
     <div className=" flex">
       <div className="text-secondaryLight rounded-lg flex flex-col w-1/3">
-        <h3 className="text-4xl font-bold m-4">Empresa</h3>
-        <p className="text-2xl p-4 m-2.5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-          reiciendis quia ratione eaque enim provident harum mollitia ipsum a.
-          Molestiae incidunt ab perspiciatis, sint porro similique nam
-          voluptatem atque odit!
-        </p>
+        <h3 className="text-4xl font-bold m-4">{props.empresa}</h3>
+        <p className="text-xl p-4 m-2.5">{props.description}</p>
       </div>
       <div className="flex justify-center flex-wrap gap-6  w-2/3">
         <img
-          className="rounded-3xl "
-          src="https://via.placeholder.com/375x300"
+          className="rounded-3xl w-[375px] h-[275]"
+          src={props.image[0]}
           alt=""
         />
         <img
-          className="rounded-3xl "
-          src="https://via.placeholder.com/375x300"
+          className="rounded-3xl w-[375px] h-[275]"
+          src={props.image[1]}
           alt=""
         />
         <img
-          className="rounded-3xl "
-          src="https://via.placeholder.com/375x300"
+          className="rounded-3xl w-[375px] h-[275]"
+          src={props.image[2]}
           alt=""
         />
         <img
-          className="rounded-3xl "
-          src="https://via.placeholder.com/375x300"
+          className="rounded-3xl w-[375px] h-[275]"
+          src={props.image[3]}
           alt=""
         />
       </div>
