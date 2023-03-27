c1a52b4c4fa4f62d80fa672d32473e3a3ffc3a5b
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-03-27 11:34:01 -0300

    Update Landing.jsx

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,13 +9,30 @@ function Landing() {
           I'm Maximiliano Permingeat Full Stack Developer.
         </h1>
         <div className="flex h-full py-5 justify-around">
-          <div className="w-1/2 flex justify-center items-center">
-            <h2 className="text-secondaryLight text-5xl my-10">
-              Lorem iphsum dolor sit, amet consectetur adipisicing elit. Amet
-              quis aut reprehenderit delectus suscipit pariatur praesentium
-              commodi placeat distinctio inventore, consequatur doloremque neque
-              enim ex doloribus nulla omnis sit molestiae!
-            </h2>
+          <div className="w-1/2 flex flex-col ">
+            <p className="text-secondaryLight text-xl m-4">
+              Sobre mí, soy Argentino. Siempre me intereso la tecnología y la
+              programación, pero empecé a estudiar programación hace 6 años.
+            </p>
+
+            <p className="text-secondaryLight text-xl m-4">
+              Uno de los cursos que realice fue el bootcamp de Henry de
+              Full-Stack Developer, un curso intensivo y con mucho que aprender.
+              Pero gracias a mi dedicación y perseverancia, pude desenvolverme
+              bien tanto el Front-end como el Back-end, aunque tengo una
+              preferencia por este último.
+            </p>
+            <p className="text-secondaryLight text-xl m-4">
+              Tengo experiencia en
+              <span className="font-bold">
+                " React, Redux, Express, node, Tailwind, Javascript,"
+              </span>
+              entre otras. Trabaje tanto en el front-end como en back-end, en
+              los proyectos que participe.
+            </p>
+            <span className=" bg-middleColor rounded-xl mt-10 p-2 text-primaryDark text-xl self-end cursor-pointer hover:font-bold hover:bg-secondaryLight">
+              Más información...
+            </span>
           </div>
           <div className="w-1/2 flex justify-center items-center">
             <img
@@ -26,7 +43,7 @@ function Landing() {
           </div>
         </div>
       </div>
-      <div className="w-3/4 ">
+      <div className="w-4/5 ">
         <h2 className=" text-primaryLight  font-bold text-6xl mt-20">
           Proyects
         </h2>
